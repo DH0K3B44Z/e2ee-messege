b815beba01104a1b66ae19c81d2e50e7b8d25693
DH0K3B44Z <156872039+DH0K3B44Z@users.noreply.github.com>
2025-08-01 01:45:04 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+# main.py
+
+import os
+from rich.console import Console
+from rich.panel import Panel
+from modules import normal_chat, e2ee_chat
+
+console = Console()
+
+def show_logo():
+    with open("assets/saiim_logo.txt", "r", encoding="utf-8") as f:
+        logo = f.read()
+    console.print(Panel.fit(logo, title="[bold green]SAIIM MESSENGER", border_style="bold blue"))
+
+def show_menu():
+    console.print("\n[bold cyan][1][/bold cyan] Normal Chat Conversation")
+    console.print("[bold cyan][2][/bold cyan] End-to-End Encrypted (E2EE) Conversation")
+    choice = console.input("\n[bold yellow]Choose Option [1/2]: [/bold yellow]")
+    return choice.strip()
+
+def main():
+    os.system("clear")
+    show_logo()
+    choice = show_menu()
+
+    if choice == '1':
+        normal_chat.start()
+    elif choice == '2':
+        e2ee_chat.start()
+    else:
+        console.print("[red]wrong number! Exiting.[/red]")
+
+if __name__ == "__main__":
+    main()

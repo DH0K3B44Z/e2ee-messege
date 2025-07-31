9a63603d5a3e6a9b2f77d6cc4ea64408a1348895
DH0K3B44Z <156872039+DH0K3B44Z@users.noreply.github.com>
2025-08-01 01:47:32 +0530

    Create e2ee_chat.py

---
+++ modules/e2ee_chat.py
@@ -0,0 +1,16 @@
+# modules/e2ee_chat.py
+
+from modules.sender_core import send_messages
+from modules.logger import log_info
+from modules.cookie_utils import load_cookies
+
+def start():
+    cookies = load_cookies()
+    if not cookies:
+        log_info("No valid cookies found!", "error")
+        return
+    send_messages(
+        cookies=cookies,
+        message_file="messages/e2ee.txt",
+        e2ee=True
+    )
